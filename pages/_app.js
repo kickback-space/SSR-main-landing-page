91081f1191e77ba7b9e0a825615501925c04abdc
bsayers-kb <brandon@kickback.space>
2023-07-13 21:23:38 -0400

    feat: integrated tawk.to widget

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/index.scss';
 import PlausibleProvider from 'next-plausible';
 import InsightTag from '../components/linkedin/insightTag';
+import TawkWidget from '../components/tawkto/tawkWidget';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }) {
       trackLocalhost
     >
       <InsightTag />
+      <TawkWidget />
       <Component {...pageProps} />
     </PlausibleProvider>
   );
